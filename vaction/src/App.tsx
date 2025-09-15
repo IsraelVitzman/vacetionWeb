832c66aa5ef9ffc1055814c5b225ec2efaa27d38
Israel Vitzman <iv058.l.v@gmail.com>
2025-09-15 16:42:16 +0300

    home page

--- vaction/src/App.tsx
+++ vaction/src/App.tsx
@@ -1,11 +1,14 @@
 import './App.css'
 
+import  LoginPage from './componenet/verificationForm/LoginPage';
+ './componenet/verificationForm/LoginPage'
+
 function App() {
  
 
   return (
     <>
-      
+      <LoginPage/>
     </>
   )
 }
